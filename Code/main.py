99a649e549cb3f8f054daf0dc70751b43a744b3f
BirdsArentRea1 <146360250+BirdsArentRea1@users.noreply.github.com>
2025-05-08 10:27:35 -0600

    Update main.py

--- Code/main.py
+++ Code/main.py
@@ -3,7 +3,7 @@ import random
 
 pygame.init()
 
-screen_width, screen_height = 1000, 800
+screen_width, screen_height = 800, 800
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("SNAKE")
 clock = pygame.time.Clock()
@@ -11,16 +11,19 @@ clock = pygame.time.Clock()
 player_image = pygame.image.load("solidsnake.webp")
 player_image = pygame.transform.scale(player_image, (50, 50))
 
+grid_size = 50
+direction = 0
+
 player_x = screen_width // 2
 player_y = screen_height // 2
-player_speed = 5
+direction = (0, 0)
 
 # Game loop ######################################################################
 def game_loop():
-    global player_x, player_y
+    global player_x, player_y, direction
     running = True
 
-    while running:
+    while running:  
         screen.fill((0, 0, 0))
 
         for event in pygame.event.get():
@@ -29,18 +32,23 @@ def game_loop():
 
         keys = pygame.key.get_pressed()
         if keys[pygame.K_LEFT]:
-            player_x -= player_speed
-        if keys[pygame.K_RIGHT]:
-            player_x += player_speed
-        if keys[pygame.K_UP]:
-            player_y -= player_speed
-        if keys[pygame.K_DOWN]:
-            player_y += player_speed
+            direction = (-grid_size, 0)
+        elif keys[pygame.K_RIGHT]:
+            direction = (grid_size, 0)
+        elif keys[pygame.K_UP]:
+            direction = (0, -grid_size)
+        elif keys[pygame.K_DOWN]:
+            direction = (0, grid_size)
+        else:
+            direction = (0, 0) 
+            
+        player_x += direction[0]
+        player_y += direction[1]
 
         screen.blit(player_image, (player_x, player_y))
-
         pygame.display.flip()
-        clock.tick(60)
+
+        clock.tick(10)
 
 # End game loop ###################################################################
 
